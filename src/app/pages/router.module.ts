fa9bd76f0a25dde78b91cb5097f60b1b28c2e7d0
Akshay Karande <59569099+akarande-wwse@users.noreply.github.com>
2021-05-15 17:44:41 +0530

    Update router.module.ts

--- src/app/pages/router.module.ts
+++ src/app/pages/router.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
